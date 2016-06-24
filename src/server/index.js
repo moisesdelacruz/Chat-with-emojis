5988d78abb0286df54d2ef711a5bf6ec019ac0ee
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-06-24 01:38:45 -0400

    Configuration of templates html en directorio view.

--- src/server/index.js
+++ src/server/index.js
@@ -9,11 +9,15 @@ const server = http.createServer(app)
 const io = secketio(server)
 
 realtime(server)
+// Configurations of views html
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
+// Configurations static files
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	res.send('hello')
+	res.render('index')
 })
 
 server.listen(port, '0.0.0.0', () => console.log(`server listening on: http:0.0.0.0:${port}`))
